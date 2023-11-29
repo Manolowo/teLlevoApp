867c8c1cbc990fbca66dce69255b335cae837cc7
Manolowo <diego001vm@gmail.com>
2023-11-29 11:01:28 -0400

    DefinitiveUpadete Map
    Actualizacion del mapa, ahora muestra la ruta desde la ubicación actual hasta el destino de la ruta.
    
    Vamooooos!

--- src/assets/js/geolocation.js
+++ src/assets/js/geolocation.js
@@ -1,21 +1,52 @@
 function mostrarUbicacionJS(latitud, longitud) {
-    // Mostrar el modal al hacer clic en el botón
-    document.getElementById('map-modal-container').removeAttribute('hidden');
-  
-    const myLatlng = new google.maps.LatLng(latitud, longitud);
-  
-    const mapOptions = {
-      center: myLatlng,
-      zoom: 20,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-    };
-  
-    const map = new google.maps.Map(document.getElementById('map-modal'), mapOptions);
-  
-    const marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Destino',
-    });
-  
+  // Obtener la ubicación actual
+  navigator.geolocation.getCurrentPosition(function (position) {
+      const currentLat = position.coords.latitude;
+      const currentLng = position.coords.longitude;
+
+      // Mostrar el modal al hacer clic en el botón
+      document.getElementById('map-modal-container').removeAttribute('hidden');
+
+      const myLatlng = new google.maps.LatLng(latitud, longitud);
+
+      const mapOptions = {
+          center: myLatlng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+      };
+
+      const map = new google.maps.Map(document.getElementById('map-modal'), mapOptions);
+
+      // Crear marcador para la ubicación del conductor
+      const driverMarker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'Destino',
+      });
+
+      // Crear marcador para la ubicación actual
+      const currentLocationMarker = new google.maps.Marker({
+          position: new google.maps.LatLng(currentLat, currentLng),
+          map: map,
+          title: 'Ubicación Actual',
+      });
+
+      // Obtener la información de la ruta
+      const directionsService = new google.maps.DirectionsService();
+      const directionsRenderer = new google.maps.DirectionsRenderer({ map: map });
+
+      const request = {
+          origin: new google.maps.LatLng(currentLat, currentLng),
+          destination: myLatlng,
+          travelMode: google.maps.TravelMode.DRIVING,
+      };
+
+      directionsService.route(request, function (response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+              directionsRenderer.setDirections(response);
+          } else {
+              console.error('Error al obtener la ruta:', status);
+          }
+      });
+  });
 }
